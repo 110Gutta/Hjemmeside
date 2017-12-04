716aee26c87843a12f5452b28027594bf3cd098a
Mats Skjærvik <mats@miriamsiniphone.uia.no>
2017-12-04 14:49:22 +0100

    La til modulside hvor man henter informasjon fra modul, som f.eks modulnavn, læringsmål og modul tekst

--- SLIT.110Gutta/src/java/Moduler/ModulServlet.java
+++ SLIT.110Gutta/src/java/Moduler/ModulServlet.java
@@ -6,13 +6,21 @@ package Moduler;
  * and open the template in the editor.
  */
 
+import DB.DataBase;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -21,32 +29,49 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/ModulServlet"})
 public class ModulServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ModulServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ModulServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+        DataBase db = new DataBase();
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        
+     
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                      throws ServletException, IOException {  
+            try {
+                response.setContentType("text/html;charset=UTF-8");
+                PrintWriter out=response.getWriter();
+                request.getRequestDispatcher("link.html").include(request, response);
 
+                HttpSession session=request.getSession(false);
+                if(session!=null){
+                String email=(String)session.getAttribute("email");
+                    
+                
+            con = db.getCon();
+            st = con.createStatement();
+            
+            String SQLhentModul = ("SELECT modulName, modulLaeringsmaal, modulTekst from Modul where modulID = 1;");
+            
+            rs = st.executeQuery(SQLhentModul);
+            
+            if(rs.next()){
+                String moduleName = rs.getString("modulName");
+                String learningGoals = rs.getString("modulLaeringsmaal");
+                String moduleText = rs.getString("modulTekst");
+            
+                out.println(moduleName + "\n");
+                out.println(learningGoals);
+                out.println(moduleText);
+                    } else{
+                    out.print("Please login first");
+                    request.getRequestDispatcher("login.html").include(request, response);
+                }  
+        out.close();
+    }
+   }        catch (SQLException ex) {
+                Logger.getLogger(ModulServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }}
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -56,34 +81,3 @@ public class ModulServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
