c0c3016036af60f2174846caaecb8a03a0a60bec
NilsBjerk <nils.f_95@hotmail.com>
2017-12-11 15:50:42 +0100

    fjerna ubrukte imports

--- SLIT.110Gutta/src/java/RegistrerBruker/Register.java
+++ SLIT.110Gutta/src/java/RegistrerBruker/Register.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import Constants.Constants;
-import java.sql.PreparedStatement;
+
  
 /**
  *
